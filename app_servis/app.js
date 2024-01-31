c83f3a2fac8031ef2360bc02e50f38436b5a5ce6
Nevena Kuzeljevic <nkuzeljevic8818rn@raf.rs>
2024-01-31 15:58:00 +0100

    Making routs for all pages for adding new

--- app_servis/app.js
+++ app_servis/app.js
@@ -6,16 +6,38 @@ const BP = require("body-parser");
 const app = express();
 //middleware
 app.use(express.static(path.join(__dirname, "static")));
+//globalni bodyparser za sve rute
+app.use(BP.urlencoded({ extended: false }));
 
 //rute
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "static", "index.html"));
 });
 
-app.use("/novo-pozoriste", BP.urlencoded({ extended: false }));
+// app.use("/novo-pozoriste", BP.urlencoded({ extended: false }));
 app.post("/novo-pozoriste", (req, res) => {
   console.log(req.body);
   res.send(req.body);
+});
+
+app.post("/nova-sala", (req, res) => {
+  res.send(req.body);
+  console.log(req.body);
+});
+
+app.post("/nova-predstava", (req, res) => {
+  res.send(req.body);
+  console.log(req.body);
+});
+
+app.post("/novi-glumac", (req, res) => {
+  res.send(req.body);
+  console.log(req.body);
+});
+
+app.post("/rezervacija", (req, res) => {
+  res.send(req.body);
+  console.log(req.body);
 });
 
 app.listen(8000);
