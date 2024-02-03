d4caf714f81e4e57ac1018ca52b808b2e68c4028
Nevena Kuzeljevic <nkuzeljevic8818rn@raf.rs>
2024-02-03 15:31:47 +0100

    Creating routes for all entities

---
+++ api_servis/routes/zanr.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const route = express.Router();
+const BP = require("body-parser");
+
+route.use(BP.urlencoded({ extended: false }));
+route.use(express.json());
+route.use(express.urlencoded({ extended: true }));
+
+//GET koji vraca sve zapise iz baze (posto smo u modulu, vec se nalazimo u /admin/pozoriste)
+route.get("/", async (req, res) => {
+  try {
+    return res.json("svi zanr");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Greska pri citanju", data: err });
+  }
+});
+
+//GET koji vraca po specificnom id-ju
+route.get("/:id", async (req, res) => {
+  try {
+    return res.json("zanr čiji je id=" + req.params.id);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Greska pri citanju", data: err });
+  }
+});
+
+//POST sa podacima u body
+route.post("/", async (req, res) => {
+  try {
+    return res.json("unos novog zanr čiji su podaci u req.body");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Greska pri unosu", data: err });
+  }
+});
+
+//PUT koji radi izmenu
+route.put("/:id", async (req, res) => {
+  try {
+    return res.json(
+      "izmena podataka zanr čiji je id=" +
+        req.params.id +
+        " a podaci su u req.body"
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Greska pri izmeni", data: err });
+  }
+});
+
+//DELETE brise po prosledjenom id-ju
+route.delete("/:id", async (req, res) => {
+  try {
+    return res.json(req.params.id); //vraca id obrisanog
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Greska pri brisanju", data: err });
+  }
+});
+module.exports = route;
